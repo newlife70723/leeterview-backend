35faec22074c2e4e2ee5cc2d15a88fb7b1b2bd67
Wei <pkh_wei23@photons.com.tw>
2025-01-24 05:34:26 +0800

    feat: update Dockerfile

--- Migrations/20250123213242_updateUserTable_20250124.Designer.cs
+++ Migrations/20250123213242_updateUserTable_20250124.Designer.cs
@@ -4,6 +4,7 @@ using LeeterviewBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace leeterview_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250123213242_updateUserTable_20250124")]
+    partial class updateUserTable_20250124
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
